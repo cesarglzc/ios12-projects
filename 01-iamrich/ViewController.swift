b3233ad9ad96b7c46465e14a801ec4397d096da0
cesarglzc <cesargonzalez@macbook-pro-de-cesar.local>
2018-08-05 23:14:30 -0500

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Cesar Eduardo Gonzalez Colunga on 05/08/18.
+//  Copyright © 2018 Cesar Eduardo Gonzalez Colunga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
